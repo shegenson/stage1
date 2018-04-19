c0d22f5a8ba3f9e2fe98ce88af346f1e4086cd52
shegenson <christyshegs@gmail.com>
2018-04-19 01:56:43 +0100

    addded my file

---
+++ index1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hnginternship</title>
+</head>
+<strong>HOTELS.NG INTERNSHIP 4.0</strong>
+<body style="background-color: Red">
+
+<div style="width: 300px;
+height: 300px;
+position: absolute;
+left: 50%;
+top: 50%;  margin-left: -150px;
+margin-top: -150px; color:#FFFFF">
+</div>
+<br>
+<br>
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
+<br>
+<br>
+<?php
+echo "Today is " . date("Y/m/d");
+?>
+</body>
+</html>
